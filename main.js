413881aea20a0631d9765469071ff8102d8d3b74
Maritxx <maritdevries1@live.nl>
2022-07-20 14:48:09 +0200

    Attempt at dropdown menu.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+function dropdown () {
+	var dropdownBox = document.getElementById("dropdownbox");
+	if (dropdownBox.style.display === "block") {
+		dropdownBox.style.display = "none";
+	} else {
+		dropdownBox.style.display = "block";
+	}
+}
